ab560dadafe44e8c80221734a6ec026f39043413
json-nguyen <39976366+json-nguyen@users.noreply.github.com>
2020-07-31 18:05:51 -0400

    Layed out logo and Analyrics Cards
    - cards still need to be styled

--- azuremonitor/client/src/components/AnalyticsWidget.js
+++ azuremonitor/client/src/components/AnalyticsWidget.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import {makeStyles} from '@material-ui/core/styles'
-import {Typography, Paper} from '@material-ui/core';
+import {Divider, Typography, Paper} from '@material-ui/core';
 
 const useStyles = makeStyles({
-    paper:{}
+    title:{
+
+    },
 })
 
 const AnalyticsWidget = (props) => {
+    const styles = useStyles()
+
     return (
-        <Paper>
-            
+        <Paper style={{padding:'10px'}} elevation={3}>
+            <div>
+                <Typography color='primary' variant='h6'>
+                    {props.title}
+                </Typography>
+            </div>
+            <Divider style={{marginBottom: '10px'}}/>
+            <div>
+                <Typography>
+                    ${props.amount}
+                </Typography>
+            </div>
+
+            <div style={{marginBottom: '10px', paddingLeft: '-10px'}} >
+                <Divider/>
+            </div>
+
+            <div>
+                <Typography>
+                    {props.description}
+                </Typography>
+            </div>
+                
         </Paper>
     )
 }
--- azuremonitor/client/src/pages/HomePage.js
+++ azuremonitor/client/src/pages/HomePage.js
@@ -9,22 +9,53 @@ const useStyles = makeStyles({
     logo: {
         maxWidth: "100%",
         height:"auto"
+    },
+   
+    widgetsContainer: {
+        marginRight:"75px",
+        display: 'flex',
+        width: '45%',
+        justifyContent: 'space-between'
+    },
+    widget: {
+        width:"100%",
+        padding: '0 10px'
     }
 })
 
 const HomePage = () => {
-
     const styles = useStyles()
+
     return (
 
         <Grid style={{padding:'10px'}}container>
-            <Grid sm={12} item>
+            <Grid style={{display:'flex'}} sm={12} item justify="space-between">
                 <div>
                     <img className={styles.logo} src={Logo} alt={"logo"} width={270}></img>
                 </div>
 
-                <div>
-
+                <div className={styles.widgetsContainer}>
+                    <div className={styles.widget}>
+                        <AnalyticsWidget 
+                            title="Current Cost" 
+                            amount={100000}
+                            description="This is the current cost of all of the current resources being used"
+                        />
+                    </div>
+                    <div className={styles.widget}>
+                        <AnalyticsWidget 
+                            title="Savings From Optimization" 
+                            amount={10000000}
+                            description="This is the current cost of all of the current resources being used"
+                        />
+                    </div>
+                    <div className={styles.widget}>
+                        <AnalyticsWidget 
+                            title="Cost after Optimization" 
+                            amount={10000000}
+                            description="This is the current cost of all of the current resources being used"
+                        />
+                    </div>
                 </div>
             </Grid>
         </Grid>
