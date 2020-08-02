29142d026b0d58a15d8c098cbcfbecfd652d8269
json-nguyen <39976366+json-nguyen@users.noreply.github.com>
2020-08-02 13:54:14 -0400

    Table done for now
    still needs to be styled and filled with data from sockets

--- azuremonitor/client/src/components/ResourcesTable.js
+++ azuremonitor/client/src/components/ResourcesTable.js
@@ -1,23 +1,85 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {makeStyles} from '@material-ui/core/styles'
-import {Table, TableContainer, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core'
-import {Paper} from '@material-ui/core'
+import {Table, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core'
+import {Box, IconButton, Collapse, Paper, Typography} from '@material-ui/core'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+
+const Row = (props) => {
+    const {row} = props
+    const [open, setOpen] = useState(false)
+    return (
+        <React.Fragment>
+            <TableRow>
+                <TableCell>
+                    {row.name}
+                    <IconButton onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/> }
+                    </IconButton>
+                </TableCell>
+                <TableCell>{row.cat1}</TableCell>
+                <TableCell>{row.cat2}</TableCell>
+                <TableCell>{row.cat3}</TableCell>
+            </TableRow>
+
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={4}>
+                    <Collapse in={open}> 
+                        <Box margin={2}>
+                        <Typography variant="h6" gutterBottom>
+                            More Data
+                        </Typography>
+                            <Table size="small">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>test 1</TableCell>
+                                        <TableCell>test 2</TableCell>
+                                    </TableRow>
+                                    
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>test 1</TableCell>
+                                        <TableCell>test 2</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    )
+}
 
 const ResourcesTable = (props) => {
+    // const {rows} = props
+    // TEMP DATA
+    let rows = [
+        {id: 1, name: "resource 1", cat1: "test1", cat2: "test2",cat3: "test3"},
+        {id: 2, name: "resource 2", cat1: "test1", cat2: "test2",cat3: "test3"},
+        {id: 3, name: "resource 3", cat1: "test1", cat2: "test2",cat3: "test3"}
+    ]
+
     return (
-        <TableContainer component={Paper}>
+        <Paper elevation={3}>
             <Table>
                 <TableHead>
                     <TableRow>
                         <TableCell>Resources </TableCell>
-                        <TableCell>Type </TableCell>
-                        <TableCell>Some Category </TableCell>
-                        <TableCell>Some Category </TableCell>
+                        <TableCell>Category 1 </TableCell>
+                        <TableCell>Category 2 </TableCell>
+                        <TableCell>Category 3 </TableCell>
 
                     </TableRow>
                 </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <Row key={row.id} row={row}/>
+                    ))}
+                </TableBody>
             </Table>
-        </TableContainer>
+        </Paper>
         
     )
 }
